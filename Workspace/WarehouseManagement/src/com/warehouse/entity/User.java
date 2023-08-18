01300785d542a05e3876859ad876115230f1cc53
Tushar Kapse <tk2172001@gmail.com>
2023-08-18 21:40:23 +0530

    getter setter deleted

--- Workspace/WarehouseManagement/src/com/warehouse/entity/User.java
+++ Workspace/WarehouseManagement/src/com/warehouse/entity/User.java
@@ -5,33 +5,5 @@ public class User {
 	private String name;
 	private String email;
 
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	@Override
-	public String toString() {
-		return "User [id=" + id + ", name=" + name + ", email=" + email + "]";
-	}
-
+	
 }
